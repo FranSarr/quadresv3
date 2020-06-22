c401ba7c000b7d759dc3eb94d9585abf841a75dc
fsarrias <Som=2018>
2020-06-22 16:07:49 +0200

    Por fin funciona el POST

--- src/main/java/com/exercicis/quadresv3/application/ShopController.java
+++ src/main/java/com/exercicis/quadresv3/application/ShopController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller // This means that this class is a Controller
+@RestController // This means that this class is a Controller
 @RequestMapping(path="/shops") //This means URL's start with /shops (after Application path)
 public class ShopController {
 
